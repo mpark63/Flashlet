6581e599080754ef183f9fa9b95850f17c282db3
mpark63 <minspk13@gmail.com>
2023-02-18 15:53:03 -0500

    changes to deploy on vercel

--- src/frontend/components/EditForm.tsx
+++ src/frontend/components/EditForm.tsx
@@ -11,14 +11,14 @@ const EditForm: FC<{
       <textarea
         className="flex w-full text-3xl rounded-xl bg-white/10 p-4 text-white text-center hover:bg-white/20"
         defaultValue={question}
-        onChange={updateQuestion}
+        onChange={(e) => updateQuestion(e)}
         rows={2}
         cols={40}
       />
       <textarea
         className="flex w-full text-3xl rounded-xl bg-white/10 p-4 text-white text-center hover:bg-white/20"
         defaultValue={answer}
-        onChange={updateAnswer}
+        onChange={(e) => updateAnswer(e)}
         rows={4}
         cols={40}
       />
