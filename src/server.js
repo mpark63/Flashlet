cf94a8613b3d659680990f2cf762c2f11ea557de
mpark63 <minspk13@gmail.com>
2023-02-18 16:49:58 -0500

    restructure

--- src/server.js
+++
@@ -1,9 +0,0 @@
-import app from "./backend/index.js";
-import * as db from "./backend/db.js";
-
-db.connect(process.env.DATABASE_URL);
-const PORT = process.env.PORT || 4567;
-
-app.listen(PORT, () => {
-  console.log(`Flashcard API at http://localhost:${PORT}/`);
-});
