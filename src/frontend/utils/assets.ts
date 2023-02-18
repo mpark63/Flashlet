873f79467ea1203094cdab5009d864fe744beba3
mpark63 <minspk13@gmail.com>
2023-02-18 15:59:43 -0500

    update getAPI

--- src/frontend/utils/assets.ts
+++ src/frontend/utils/assets.ts
@@ -3,4 +3,4 @@ import { Flashcard } from "./commonTypes";
 
 export const getAPI = (window) =>
   window.location.href.includes('http://localhost:3000')
-    ? 'http://localhost:4567' : 'http://localhost:4567'; 
+    ? 'http://localhost:4567' : 'https://flashlet-api.onrender.com'; 
